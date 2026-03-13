201281ac426d3e5c02dce00fdcaed3037e2b4663
Cosereanu Emanuel <77057756+emanuelco07@users.noreply.github.com>
2026-03-13 17:45:54 +0200

    adding main functionalitis such as multiplication, sum, and div

---
+++ index.js
@@ -0,0 +1,24 @@
+// Calculator program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
+
+function deleteLastItem() {
+ display.value = display.value.slice(0, -1); //returns the string without the last character
+}
